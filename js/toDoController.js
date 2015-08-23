c7ac8f33e53cacf617ab43f26fcfdf72d778bb03
andygout <andygout@hotmail.co.uk>
2015-08-23 15:03:06 +0100

    Add: Add tasks functionality w/Protractor tests

--- js/toDoController.js
+++ js/toDoController.js
@@ -1,3 +1,16 @@
 toDoList.controller('ToDoListController', [function() {
 
+  var self = this;
+
+  self.taskArray = []
+
+  self.addTask = function() {
+    if(self.newTask) {
+      var task = {"descr": self.newTask,
+                  "complete": false}
+      self.taskArray.push(task);
+      self.newTask = '';
+    }
+  }
+
 }]);
--- test/e2e/toDoListFeature.js
+++ test/e2e/toDoListFeature.js
@@ -1,5 +1,9 @@
 describe('To Do List creator', function() {
 
+  var entryField = element(by.model('toDoCtrl.newTask'));
+  var addButton = element(by.id('addButton'));
+  var tasks = element.all(by.repeater('task in toDoCtrl.taskArray'));
+
   beforeEach(function() {
     browser.get('http://localhost:8080');
   })
@@ -7,5 +11,32 @@ describe('To Do List creator', function() {
   it('has a title', function() {
     expect(browser.getTitle()).toEqual('To Do List');
   })
+
+  it('can add and display a task', function() {
+    addTasks(['practise violin']);
+    expect(tasks.first().getText()).toContain('practise violin');
+  });
+
+  it('can add and display a task', function() {
+    addTasks(['practise violin']);
+    expect(tasks.first().getText()).toContain('practise violin');
+  });
+
+  it('can add and display two tasks in ascending chronological order', function() {
+    addTasks(['practise violin', 'buy potatoes']);
+    expect(tasks.last().getText()).toContain('buy potatoes');
+  });
+
+  it('can count number of tasks', function() {
+    addTasks(['practise violin', 'buy potatoes']);
+    expect(tasks.count()).toEqual(2);
+  })
+
+  var addTasks = function(tasks) {
+    tasks.forEach(function(task) {
+      entryField.sendKeys(task);
+      addButton.click();
+    });
+  };
 
 })
